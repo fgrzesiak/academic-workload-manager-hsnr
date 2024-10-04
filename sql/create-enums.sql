2a61443004f07280dd9209245ed5927978a7edbf
B4ZZ3 <develop.christel@gmail.com>
2024-10-04 10:10:13 +0200

    add: enum values for tables

---
+++ sql/create-enums.sql
@@ -0,0 +1,19 @@
+-- insert specified values for reduction, supervision and semester
+INSERT INTO Semester (Name, IsActive) VALUES 
+('SS22', 0), 
+('WS2223', 0), 
+('SS23', 0), 
+('WS2324', 0), 
+('SS24', 0), 
+('WS2435', 1);
+
+INSERT INTO TypeOfSupervision (TypeOfSupervision, CalculationFactor) VALUES 
+('Bachelorarbeit', '0.3'), 
+('Masterarbeit', '0.3'), 
+('Zweitprüfer', '0.1'), 
+('Praxissemester', '0.2');
+
+INSERT INTO TypeOfReduction (TypeOfReduction) VALUES 
+('Funktion/Aufgabe'), 
+('Forschung/Entwicklung'), 
+('Behinderung');
