154d0968be005151790157bc37d15e80ed32213e
Bjarne Christel <develop.christel@gmail.com>
2024-11-08 14:54:50 +0100

    add: first select satements

---
+++ sql/select.sql
@@ -0,0 +1,78 @@
+SET @user = "mafre001";
+SET @semester = 8;
+
+-- Deputat for specific prof for specific semester
+SELECT Semester.Name AS Semester, DeputationPerSemester.DeputationIndividual AS Deputat  
+FROM DeputationPerSemester 
+INNER JOIN Semester 
+ON DeputationPerSemester.SemesterID = Semester.SemesterID
+AND DeputationPerSemester.Teacher = @user
+AND DeputationPerSemester.SemesterID = @semester;
+
+-- overview of group of supervision
+SELECT TypeOfSupervision.TypeOfSupervision, SUM(TypeOfSupervision.CalculationFactor) AS CalculationFactor
+FROM Supervision
+INNER JOIN TypeOfSupervision 
+ON Supervision.TypeOfSupervisionID = TypeOfSupervision.TypeOfSupervisionID
+AND Supervision.Teacher = @user
+AND Supervision.SemesterID = @semester
+GROUP BY TypeOfSupervision.TypeOfSupervision;
+
+SELECT SUM(TypeOfSupervision.CalculationFactor) AS SumSupervision
+FROM Supervision
+INNER JOIN TypeOfSupervision 
+ON Supervision.TypeOfSupervisionID = TypeOfSupervision.TypeOfSupervisionID
+AND Supervision.Teacher = @user
+AND Supervision.SemesterID = @semester;
+
+-- overview of lectures
+SELECT SUM(HoursSWS) AS SumLecture
+FROM Lecture
+WHERE Lecture.Teacher = @user
+AND Lecture.SemesterID = @semester;
+
+-- overview of reduction
+SELECT SUM(ScopeOfReduction) AS SumReduction
+FROM Reduction
+WHERE Reduction.Teacher = @user
+AND Reduction.SemesterID = @semester;
+
+-- overview auf saldo
+SELECT 
+    (SELECT SUM(ts.CalculationFactor)
+     FROM Supervision s
+     INNER JOIN TypeOfSupervision ts
+     ON s.TypeOfSupervisionID = ts.TypeOfSupervisionID
+     WHERE s.Teacher = @user
+     AND s.SemesterID = @semester) AS SumSupervision,
+     
+    (SELECT SUM(l.HoursSWS)
+     FROM Lecture l
+     WHERE l.Teacher = @user
+     AND l.SemesterID = @semester) AS SumLecture,
+    
+    (SELECT SUM(r.ScopeOfReduction)
+     FROM Reduction r
+     WHERE r.Teacher = @user
+     AND r.SemesterID = @semester) AS SumReduction,
+
+-- overview of saldo
+SELECT (SELECT SUM(ts.CalculationFactor)
+     FROM Supervision s
+     INNER JOIN TypeOfSupervision ts
+     ON s.TypeOfSupervisionID = ts.TypeOfSupervisionID
+     WHERE s.Teacher = @user
+     AND s.SemesterID = @semester) +
+    (SELECT SUM(l.HoursSWS)
+     FROM Lecture l
+     WHERE l.Teacher = @user
+     AND l.SemesterID = @semester) +
+    (SELECT SUM(r.ScopeOfReduction)
+     FROM Reduction r
+     WHERE r.Teacher = @user
+     AND r.SemesterID = @semester) -
+    (SELECT d.DeputationIndividual   
+     FROM DeputationPerSemester d
+     AND d.Teacher = @user
+     AND d.SemesterID = @semester) AS SumSaldo;
+
