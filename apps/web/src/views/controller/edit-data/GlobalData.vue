59c104db4ffbbc309746751de5bed34291be3e64
Bjarne Christel <develop.christel@gmail.com>
2024-12-18 17:07:12 +0100

    add: layout idea for global data page

---
+++ apps/web/src/views/controller/edit-data/GlobalData.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card">
+        <h1 class="mb-4 text-xl font-semibold">Stammdatenverwaltung</h1>
+    </div>
+    <div class="card">
+        <h2 class="mb-4 text-l font-semibold">Semester</h2>
+        <p>Semester anlegen und bearbeiten (aktives Semester für Datenübertragung setzen/ als abgeschlossen markieren)</p>
+    </div>
+    <div class="grid grid-cols-2 gap-4 ">
+        <div class="card col-span-1">
+            <h2 class="mb-4 text-l font-semibold">Betreuungsarten + Anrechnungsfaktor</h2>
+            <p>Nebeneinander Tabellen mit SupervisionType und DiscountType</p>
+        </div>
+        <div class="card col-span-1">
+            <h2 class="mb-4 text-l font-semibold">Ermäßigungsarten</h2>
+        </div>
+    </div>
+</template>
+<script setup lang="ts"></script>
